9885701af9da072ee13addd28bae6ad4925d81c9
Marifel Barbasa <mrbarbasa@gmail.com>
2022-04-20 08:56:15 -1000

    Remove the links to Kaggle

--- pelicanconf.py
+++ pelicanconf.py
@@ -27,7 +27,6 @@ RSS_FEED_SUMMARY_ONLY = False
 # Blogroll
 LINKS = (
     ('GitHub', 'https://github.com/mrbarbasa'),
-    ('Kaggle', 'https://www.kaggle.com/mrbarbasa'),
     ('LinkedIn', 'https://www.linkedin.com/in/mrbarbasa'),)
 
 # Uncomment following line if you want document-relative URLs
